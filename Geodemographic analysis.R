79b0951ee16b22dba0f5bc870eb12cdb80965db4
Robert <ziomas1@hotmail.com>
2018-05-12 18:37:53 -0400

    Update ideas for analysis

--- Geodemographic analysis.R
+++ Geodemographic analysis.R
@@ -1,2 +1,7 @@
-# Ideas for analysis
+# Ideas for analysis ------------------------------------------------------
 # shiny app for exploring the dataset, maybe pull some ideas from Radiant
+# use gmailr to design and send a simple email marketing campaign
+#   maybe design a sample tempate with a mock coffee store, 
+#   would be all on a separate tab
+
+
